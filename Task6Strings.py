8cc19d1b7785e9ccedfd585b6884db0c4af51064
Alnafis Chowdhury <99893321+RacingXHunter@users.noreply.github.com>
2022-08-09 18:21:33 +0100

    3rd

--- Task6Strings.py
+++ Task6Strings.py
@@ -1,12 +1,45 @@
-x = "There are %d types of people. " % 10
+types_of_people = 10
+
+x = f"there are {types_of_people} types of people"
+
 binary = "binary"
 do_not = "don't"
-y = "Those who know %s and those who %s" % (binary, do_not)
-
+y = f"those who know {binary} and those who {do_not}."
 
 print(x)
 print(y)
 
-print("I said: %r. " % x)
+print(f" i said: {x}")
+print(f"i also said: '{y}'")
 print("")
-print("I also said '%s'. " % y)
+#Space == Tryna false/true q's
+hilarious = False
+
+joke_evaluation = "Isnt that joke so funny ?! {}"
+
+print(joke_evaluation.format(hilarious))
+print("")
+
+w = "this is the left side of ..."
+e = "a string with the right side."
+#Well the reason why adding 2 string makes it longer is because we are adding var W and var E Combine!
+
+print(w+e)
+
+#Let's try some ourselves!!
+
+calx = 10
+caly = 20
+
+x = f"there are {calx} number in calculator"
+y = f"there are less than {caly} number in calculator"
+z = f"combining x+y is {x} {y}"
+
+print("----------")
+print(x)
+print("")
+print(y)
+print("")
+print(z)
+print(x + " " + y) #Works in both ways x, y or + " " + 
+
---
+++ Task7MorePrinting.py
@@ -0,0 +1,30 @@
+print("Mary had a little lamp.")
+print("it's fleece was a whiye as {}." .format('snow'))
+print("hi my name is: {}" .format("Alnafis"))
+print("." * 20) #Dayuuuuuuuum okay 200iQ
+
+end1 = "C"
+end2 = "H"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+end14 = "3"
+bend1 = "44"
+bend2 = "44"
+
+print("")
+print (end1 + end2 + end3 + end4 + end5 + end6, end= " ")
+print (end7 + end8 + end9 + end10 + end11 + end12, end= " ")
+print("hello")
+print("")
+
+
+print (end1, end = " ")
+print("yeyeyw")
