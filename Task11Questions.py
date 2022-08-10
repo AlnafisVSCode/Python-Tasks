7ce3d00471564c6207c4e54c820449e7e70d5be3
Alnafis Chowdhury <99893321+RacingXHunter@users.noreply.github.com>
2022-08-10 21:28:25 +0100

    Prompts and parameters packing

---
+++ Task11Questions.py
@@ -0,0 +1,23 @@
+print("How old are you?", end=" ")
+age = input()
+# print(age)
+
+print("How tall are you Brudda?", end= " ")
+height = input()
+
+print("How much do you weigh?", end=" ")
+weight = input()
+
+
+    #Space - Doing Math - Try Out's Let's goooooo
+print("Input A:", end=" ")
+Timex = int(input())
+print("Input B:", end=" ")
+Timey = int(input())
+
+
+
+
+print("So, you are:", age, "years old and", height, "ft tall and", weight, "kg heavy")
+print(f"So you are {age} years old and {height} tall and also {weight} kg heavy")
+print("So the Total Multiplied answer is:", Timex * Timey)
---
+++ Task12Prompts.py
@@ -0,0 +1,11 @@
+y = input("Name? ")
+# print(y)
+print("How much do you weigh?", end=" ")
+weight = input()
+#Space
+print("")
+age = input("How old are you? ")
+height = input("How tall are you? ")
+light = input("How much bright? ")
+
+print(f"so you are {age} years old, at a decent height as {height} but also {light} into the future!! NICE!!")
