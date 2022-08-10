05e609866d6baacca98367f62b8a987407ac4041
Alnafis Chowdhury <99893321+RacingXHunter@users.noreply.github.com>
2022-08-10 13:32:36 +0100

    Task11

---
+++ 0Tracker.py
@@ -0,0 +1,39 @@
+from time import time
+
+#Calculate your Time!
+
+#In Minutes
+day1 = 158
+day2 = 0
+day3 = 0
+day4 = 0
+day5 = 0
+day6 = 0
+day7 = 0
+
+total_time = day1+day2+day3+day4+day5+day6+day7
+time_remaining = (840-total_time)
+hour_remaining = (time_remaining / 60)
+#Space
+print("Daily Information\n", "Day1: {} Mins\n" .format(day1),
+        "Day2: {} Mins\n".format(day2),
+        "Day3: {} Mins\n" .format(day3),
+        "Day4: {} Mins\n" .format(day4),
+        "Day5: {} Mins\n" .format(day5),
+        "Day6: {} Mins\n" .format(day6),
+        "Day7: {} Mins\n" .format(day7))
+        
+
+
+#mylist = [day1, day2, day3, day4, day5, day6, day7]
+   # for i in mylist:
+  #      print(i)
+ #       
+
+
+#print(day1,day2 ,day3,day4,day5,day6,day7)
+print("Total Time Spent:", total_time)
+print(f"Time Remaining: {time_remaining} Minutes")
+print("Hours Remaining: ", hour_remaining)
+print("Helper:", 120+ 38 )
+
---
+++ Task10Slashes.py
@@ -0,0 +1,29 @@
+tabby_cat = "\tI'm tabbing in."
+hey = "hello"
+persian_cat = "I'm  \rsplit\non a line."
+backslash_cat = "I'm \\ a \\ cat??"
+#Space
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishing
+\t* Catnip\n\t* Grass
+"""
+#Space - Try Out
+x = "Hello it's me Brudda 'okkk' nahhh"
+y= '''
+I'll do a list:
+\t* Cat food
+\t* Fishing
+\t* Catnip\n\t* Grass
+'''
+
+#\t == Indent forward same as TAB Push
+
+print(tabby_cat)
+print(hey)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+# print(x)
+print(y)
