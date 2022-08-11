fd60c4ee3c56ed9d9aa87c67ed0b3f0bf906aefc
Alnafis Chowdhury <99893321+RacingXHunter@users.noreply.github.com>
2022-08-11 18:14:37 +0100

    Reading Files

---
+++ Task15ReadingFiles.py
@@ -0,0 +1,39 @@
+from sys import argv
+
+# # print("What is the File Name BOY")
+# #Creating the arguament and it allows us to input using our script argv
+# script, filename= argv
+
+# #open() opens the file and it stores it in the var!
+# txt = open(filename)
+
+# print(f"Here is you file {filename}:")
+# print(txt.read()) #it reads the file which it then prints using the print 
+
+# #Looops
+# print("Type the file name again:")
+# file_again = input("> ")
+
+# txt_again = open(file_again)
+# # print(txt_again.read()) It will read the whole thing
+
+# #https://www.digitalocean.com/community/tutorials/how-to-handle-plain-text-files-in-python-3
+# #Space - Read One Line!!
+# print(txt_again.readline()) 
+
+# print(txt_again.readline())
+# #Read all each Lines!!!!
+# # print(txt_again.readlines())
+
+title = 'fllllllllllllllllllllllllpalpefpakfoakfoksofe'
+
+script, filename = argv
+# file_again = "ex15_sample.txt"
+txt = open(filename)
+print(f"Here is you file {filename}:")
+print(txt.read()) 
+
+filename.write("title")
+# print(title)
+
+filename.close()
