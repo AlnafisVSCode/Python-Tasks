3b8dd56f106e4d0ec31bbbe595901571ef04f86a
Alnafis Chowdhury <99893321+RacingXHunter@users.noreply.github.com>
2022-08-11 22:29:25 +0100

    Finally Writing Successfully

--- Task16Writing.py
+++ Task16Writing.py
@@ -10,8 +10,27 @@ print("If you do want that, hit RETURN.")
 input("?")
 
 print("Opening the file...")
-targer = open(filename, 'w')
+target = open(filename, 'w')
 
 print("Truncating the file. Gooooooooodbye")
-target.tra
+# target.trancate()
+
+print("Now Im going to ask you for three lines.")
+
+line1 = input("Line 1: ")
+line2 = input("Line 2: ")
+line3 = input("Line 3: ")
+
+print("I'm going to write these to the file.")
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+print("And we are finally, Closing this shhhhhhhhhhit Twinn.")
+target.close()
+
 
