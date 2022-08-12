e12128d6cde1852e89ec34aab7a0b580baf3dbe8
Alnafis Chowdhury <99893321+RacingXHunter@users.noreply.github.com>
2022-08-12 16:51:02 +0100

    MoreFiles

--- Task16Writing.py
+++ Task16Writing.py
@@ -23,18 +23,24 @@ line3 = input("Line 3: ")
 
 print("I'm going to write these to the file.")
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+# target.write(line1)
+# target.write("\n")
+# target.write(line2)
+# target.write("\n")
+# target.write(line3)
+# target.write("\n")
+
+##SOLOOO!!!!!!! So Much Efficiency
+target.write(f"{line1}\n{line2}\n{line3}\n")
+
 
 print("And we are finally, Closing this shhhhhhhhhhit Twinn.")
 target.close()
 #Now We Gon Try Our selves Creating what we did previosuly on our ownn Twin
 
 #Also Comment out before last Comment as it's the real Example but leave this onward for us
+
+#It's On Task16 Solo Page
 
 
 
