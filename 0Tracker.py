ad93a74fe462bf860a45ee7a17ce5d115cde4b36
Alnafis Chowdhury <99893321+RacingXHunter@users.noreply.github.com>
2022-08-18 17:44:45 +0100

    Tracker Reset

--- 0Tracker.py
+++ 0Tracker.py
@@ -2,17 +2,22 @@ from time import time
 
 #Calculate your Time!
 
+#Weeks 1-4 > Mins
+Week_1 = 886  # 9 - 18 Aug
+Week_2 = 0    #18 - 25 Aug
+Week_3 = 0
+
 #In Minutes
 day1 = 158
 day2 = 158
 day3 = 120
 day4 = 120
 day5 = 120
-"Wednesday"
 day6 = 120
-day7 = 0
+day7 = 90
 
 total_time = day1+day2+day3+day4+day5+day6+day7
+total_timeH = total_time/60
 time_remaining = (840-total_time)
 hour_remaining = (time_remaining / 60)
 #Space
@@ -33,7 +38,7 @@ print("Daily Information\n", "Day1: {} Mins\n" .format(day1),
 
 
 #print(day1,day2 ,day3,day4,day5,day6,day7)
-print("Total Time Spent:", total_time)
+print("Total Time Spent:", total_time, ">", total_timeH, "Hours")
 print(f"Time Remaining: {time_remaining} Minutes")
 print("Hours Remaining: ", hour_remaining)
 print("Helper:", 120+ 38 )
