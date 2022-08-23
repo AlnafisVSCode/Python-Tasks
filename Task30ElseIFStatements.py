2e08eec74d7f93cdb28214a5a47bc166857f8335
Alnafis Chowdhury <99893321+RacingXHunter@users.noreply.github.com>
2022-08-23 13:39:00 +0100

    Else Statements

---
+++ Task30ElseIFStatements.py
@@ -0,0 +1,11 @@
+people = 30
+cars = 30
+trucks = 15
+
+
+if cars > people:
+    print("We should take the cars.")
+elif cars < people:
+    print("We should NOT take the cars.")
+else:
+    print("We can't decide.")
