40940d72f22b3d321c6d95c4c139b762f630eaf2
Alnafis Chowdhury <99893321+RacingXHunter@users.noreply.github.com>
2022-08-23 13:08:19 +0100

    Booleans

---
+++ Task28Boolean.py
@@ -0,0 +1,25 @@
+from tokenize import Triple
+
+
+print(True and True)
+print(False and True)
+print(1==1 and 2==1)
+print("test" == "test")
+print(1==1 or 2!=1)
+print(False and 0 != 0)
+print(True or 1 == 1)
+print("test" == "Testing")
+print(1 != 0 and 2 ==1)
+print("Tets" != "Testing")#11
+print("t" == 1)
+print(not (True and False))
+print(not (1 == 1 and 0 != 1))
+print(not (10 == 1 or 1000 == 1000))#15
+print(not (1 !=10 or 3 == 4 ))
+print(not ("testing" == "testing" and "zed" == "Cool guy"))
+print(1 == 1 and (not ("testing" == 1 or 1 == 0)))#18
+print("chunky" == "bacon" and (not(3 == 4 or 3 == 3)))#19
+print(3 == 3 and (not ("testing" == "testing" or "python" == "fun")))#20
+
+
+
