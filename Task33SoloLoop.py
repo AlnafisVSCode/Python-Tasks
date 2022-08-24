74d546ab9ff6992c3a7093909d4d8efa20c38e13
Alnafis Chowdhury <99893321+RacingXHunter@users.noreply.github.com>
2022-08-24 14:50:20 +0100

    Individual Loops

---
+++ Task33SoloLoop.py
@@ -0,0 +1,72 @@
+
+
+print("You just joined the game!!!")
+print("Name?")
+# user = input(": ")
+# print(f"allright {user}. Choose Door A or B?")
+print("""Door A: 1
+Door B: 2""")
+
+door_1 = ["A","B","C"]
+door_2 = [1,2,3,4,5]
+door_3= []
+
+# for door in door_2:
+#     print(f"Keys are: {door}")
+
+    # door = door + 1
+
+
+# for empty_door in door_3:
+#     print("Added:", empty_door)
+
+#     empty_door = empty_door +1
+#     empty_door.append(door_3)
+"-----------------------------------------"
+#Appending number to a list
+
+# elements = []
+
+# for i in range(0, 7):
+#     print(f"Printing {i}")
+
+#     elements.append(i)
+
+#     print(elements)
+
+# for b in elements:
+#     print("Number:",b,"Has been added")
+
+"--------------------------------------------"
+# print("")
+# for letters in door_1:
+#     print("The letters we included are:")
+
+#     for i in range(0, 7):
+#         print(f"{i} added to the list!")
+#         door_1.append(i)
+# print(door_3)
+        # for x in door_1:
+        #     print("Printed all characters in the list:")
+        #     print(x)
+
+"--------------------------------------------------------------"
+print("")
+                                                                             #While Loop[s]
+
+elements = []
+colors = ["blue", "Red", "Yellow"]
+xcoin = 1
+
+while xcoin <= 100:
+    print("Added:", xcoin, "xcoin")
+    elements.append(xcoin)
+    xcoin = xcoin + xcoin 
+   
+
+for x in elements:
+    print(x,"th")
+
+
+
+
