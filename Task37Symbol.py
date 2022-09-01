018be86b213c8ace7b7c88d815108119292ddc1a
Alnafis Chowdhury <99893321+RacingXHunter@users.noreply.github.com>
2022-09-01 20:54:34 +0100

    keywords

--- Task37Symbol.py
+++ Task37Symbol.py
@@ -9,45 +9,89 @@
 
 
 
+
+
 arrow = "> "
-def Duungeon():
-    print("Welcome to the game that feels never ending!")
-    print("Guess the number to go to level 2 or FF")
-    up_five = ["1","2","3","4","5"]
-    down_five = 1, 2, 3
-    loop = False
-    x = 6
+# def Duungeon():
+#     print("Welcome to the game that feels never ending!")
+#     print("Guess the number to go to level 2 or FF")
+#     up_five = ["1","2","3","4","5"]
+#     down_five = 1, 2, 3
+#     loop = False
+#     x = 6
 
-    while True:
-        print("\nRANGE FROM : 1 - 10")
+#     while True:
+#         print("\nRANGE FROM : 1 - 10")
 
-        choice = int(input(arrow))
-        if choice == x:
-            print("GAMEOVER------------------------------------------>")
-            dead()
-        elif choice == 9 and not loop:
-            print("Nice One. You can Proceed to Lv 2.")
-            print("jk")
-            loop = True
-        elif choice == 9 and loop:
-            print("Nice One. You can Proceed to Lv 2.")
-            level_2()
-            break
-            # break
-        # elif choice != 9:
-        #     print("You Lost\n GO Again!..?")
+#         choice = int(input(arrow))
+#         if choice == x:
+#             print("GAMEOVER------------------------------------------>")
+#             dead()
+#         elif choice == 9 and not loop:
+#             print("Nice One. You can Proceed to Lv 2.")
+#             print("jk")
+#             loop = True
+#         elif choice == 9 and loop:
+#             print("Nice One. You can Proceed to Lv 2.")
+#             level_2()
+#             break
+#         # elif choice != 9:
+#         #     print("You Lost\n GO Again!..?")
 
-        else:
-            print("Go Aggane")
-        # else:
-        #     print("You Lost\n GO Again!..?")
-        #     break
+#         else:
+#             print("Go Aggane")
+#         # else:
+#         #     print("You Lost\n GO Again!..?")
+#         #     break
     
+# def level_2():
+    
+#     print(" ")
+#     print("----->>>>________Welcome To Level 2...________<<<<<<-----")
+#     print("Welcome to Level 2....")
+#     print("It's a Math Game\n What is: 2+2+6/2+2+1?")
+#     for i in range(10):
+#         choice = int(input(arrow))
 
-def dead():
-    print("gOne")
+#         if choice <= 3:
+#             print("Are you Freaking Dump?")
+            
+#         elif choice <= 6 and choice > 3:
+#             print("Your Still Dump")
+            
+        
+#         elif choice <= 9 and choice >= 6:
+#             print("Nahhhhhh Cmon Dudeeeeee")
+            
+#         elif choice == 10:
+#             print("wELL Done...\nUr Smart!")
+#             # continue
+#             # level_3()
 
-def level_2():
-    print("Welcome to level 2")
+#         else:
+#             print("I think that's not the range we asked you Asshole!")
 
-Duungeon()
+
+def if_statement():
+    print("Print 1 to win or any else to lose ")
+
+    while True: 
+        choice = input()
+        if "77" in choice:
+            print("Win")
+            # continue
+        else:
+            print("Lose")
+            
+
+if_statement()
+
+# def dead():
+#     print("gOne")
+
+# level_2()
+# def level_3():
+#     print("Welcome to Level 3")
+
+
+# Duungeon()
