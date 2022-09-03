4bf8e0889f2a6ff58edd1df824d60afeb72750fd
Alnafis Chowdhury <99893321+RacingXHunter@users.noreply.github.com>
2022-09-03 22:29:29 +0100

    Dictionary Intro

--- 0Tracker.py
+++ 0Tracker.py
@@ -10,13 +10,13 @@ Week_4 = 0       #04 - 10 Sep
 Week_5 = 0
 
 #In Minutes
-day1 = 152 #25
-day2 = 120 #26
-day3 = 120 #27
-day4 = 0        #28
-day5 = 121
-day6 = 173
-day7 = 120
+day1 = 50       #04
+day2 = 0        #05
+day3 = 0        #06
+day4 = 0        #07
+day5 = 0
+day6 = 0
+day7 = 0        #10
 
 total_time = day1+day2+day3+day4+day5+day6+day7
 total_timeH = total_time/60
