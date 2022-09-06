9aa8710ae2e1cea997514a769e750f4d0b0cf880
Alnafis Chowdhury <99893321+RacingXHunter@users.noreply.github.com>
2022-09-06 20:41:06 +0100

    Object Oriented Programming

--- Task40Modules.py
+++ Task40Modules.py
@@ -1,5 +1,41 @@
-import Task39OOP
+class Song(object):
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def runSong(self):
+        for line in self.lyrics:
+            print(line)
 
 
 
-print(Task39OOP.apple.apples())
+"-------------------------------------------------------"
+happy_dbay = Song(["Happy birthday to you",
+"I don't want to get sued", "So i'll stop right here!"])
+
+bulls_on_parade = Song(["They rally around the family",
+ "With pockets full of shells"])
+"-------------------------------------------------------"
+
+# lol = happy_dbay.lyrics
+# print(lol)
+happy_dbay.runSong()
+
+bulls_on_parade.runSong()
+
+
+# class Emplyees():
+
+
+#     def __init__(self, first, last, pay):
+#         self.first = first
+#         self.last = last
+#         self.pay = pay
+#         self.forecastCalculator = self.pay * 3
+
+
+#  #Alot of data here:
+# customer1 = Emplyees("Bob", "Lanny", 50000)
+# customer2 = Emplyees("Curry", "Lane", 10000)
+
+# print(customer1.first)
+# print(customer2.forecastCalculator)
