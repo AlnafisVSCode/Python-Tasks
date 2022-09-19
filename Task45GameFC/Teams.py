ea875c118fe358f2354739c62b30daf9ec40c6e4
Alnafis Chowdhury <99893321+RacingXHunter@users.noreply.github.com>
2022-09-19 15:21:43 +0100

    Project Solo

---
+++ Task45GameFC/Teams.py
@@ -0,0 +1,25 @@
+FCTeams = {
+        "Liverpool":"Liverpool",
+        "Arsenal":"Arsenal",
+        "Manchester United":"Manchester United",
+        "Chelsea":"Chelsea",
+        "Everton":"Everton",
+        "Fulham":"Fulham",
+        "Leeds":"Leeds",
+        "Wolves":"Wolves"
+}
+
+# def e():
+#     FCClubs = ['Liverpool', 'Arsenal', 'Manchester United', 
+#         'Chelsea', 'Everton', 'Fulham', 'Leeds', 'Wolves']
+
+#     FCdata = {}
+
+#     # for e in FCClubs:
+#     #     FCdata[e] = FCdata.get(e,0)+1
+
+#     # print(FCdata)
+
+#     count = {i:FCClubs.count(i) for i in FCClubs}
+
+#     print(count)
